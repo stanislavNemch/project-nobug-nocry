bd6986530fbf99d75ebdd3b37c5fdb22a7164db5
abrosenkov <abrosenkov91@gmail.com>
2025-08-07 17:30:01 +0200

    add category

--- src/js/3-product.js
+++ src/js/3-product.js
@@ -4,3 +4,42 @@ import {
   getFeedbacks,
   createOrder,
 } from './furniture-api.js';
+
+const listCategory = document.querySelector('.product-categories-list');
+
+export function renderCategories(data) {
+  const markup = data
+    .map(
+      el => `<li class="product-categories-item">
+      ${el.name}
+      </li>`
+    )
+    .join('');
+
+  listCategory.innerHTML = markup;
+}
+
+async function getAllCategories() {
+  try {
+    const data = await getCategories();
+    renderCategories([{ name: 'Всі товари' }, ...data]);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+// async function renderCategories() {
+//   try {
+//     const data = await listCategory();
+//     const murkUp = data.map(elem => {
+//       return elem;
+//     });
+//     console.log(murkUp);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+getAllCategories();
